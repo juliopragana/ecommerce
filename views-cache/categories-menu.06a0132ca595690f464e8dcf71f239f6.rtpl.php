30d7cc15049cb8fddc0e20118b93bd5b1a6e5e52
juliopragana <julinhopragana@gmail.com>
2018-07-16 23:29:48 -0300

    Site categorias

---
+++ views-cache/categories-menu.06a0132ca595690f464e8dcf71f239f6.rtpl.php
@@ -0,0 +1,1 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/2">Apple</a></li><li><a href="/categories/5">LG</a></li><li><a href="/categories/6">Motorola</a></li><li><a href="/categories/7">Samsung</a></li>
